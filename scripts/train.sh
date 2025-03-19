2a8239557b1a6fbb8406a10bb59803ef5c3f07b2
Michael Hu <myhu.lilac@gmail.com>
2025-03-19 15:50:12 -0400

    comment edit

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,4 @@
 # Note: This script pretrains on wikitext for easy downloading.
-# The gap between pre-pretraining and no pre-pretraining is smaller in this setting,
-# likely because wikitext is small enough to do multiple epochs within 3000 steps.
 # To reproduce paper results, download and tokenize c4.
 
 # pre-pretrain
@@ -11,4 +9,4 @@ python train.py --model_name EleutherAI/pythia-160m --data_dir ./data/tokenized/
 python train.py --model_name EleutherAI/pythia-160m --data_dir ./data/tokenized/wikitext --output_dir ./output/wikitext/pythia-160m --save_steps 3000 --max_steps 3000 --reinit True
 
 # pretrain w shuff_dyck
-python train.py --model_name ./output/shuff_dyck/pythia-160m/checkpoint-500 --data_dir ./data/tokenized/wikitext --output_dir ./output/wikitext/pythia-160m --save_steps 3000 --max_steps 3000 --reinit False 
+python train.py --model_name ./output/shuff_dyck/pythia-160m/checkpoint-500 --data_dir ./data/tokenized/wikitext --output_dir ./output/wikitext/pythia-160m/shuff_dyck --save_steps 3000 --max_steps 3000 --reinit False 
